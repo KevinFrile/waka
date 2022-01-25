2f5248e27617ea528bf4943deb9b336d2fcd13f8
Kevin Frile <kafraile@misena.edu.co>
2022-01-24 21:59:49 -0500

    v0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,35 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'tabs/mapa',
+    pathMatch: 'full',
+  },
+  {
+    path: 'mapa',
+    loadChildren: () => import('./pages/mapa/mapa.module').then( m => m.MapaPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'info-parqueadero',
+    loadChildren: () => import('./pages/info-parqueadero/info-parqueadero.module').then( m => m.InfoParqueaderoPageModule)
+  },
+  {
+    path: 'recibo',
+    loadChildren: () => import('./pages/recibo/recibo.module').then( m => m.ReciboPageModule)
+  },
+  {
+    path: 'info-vehiculo',
+    loadChildren: () => import('./pages/info-vehiculo/info-vehiculo.module').then( m => m.InfoVehiculoPageModule)
   },
 ];
 
---
+++ src/app/pages/mapa/mapa.page.ts
@@ -0,0 +1,51 @@
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { IonRouterOutlet, ModalController } from '@ionic/angular';
+import { InfoVehiculoPage } from '../info-vehiculo/info-vehiculo.page';
+import { MapboxService } from '../../services/mapbox.service';
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.page.html',
+  styleUrls: ['./mapa.page.scss'],
+})
+export class MapaPage implements OnInit {
+
+  constructor(private routerOutlet: IonRouterOutlet, private modalController: ModalController, private servicioMapBox: MapboxService, private renderer: Renderer2) {
+
+
+  }
+
+  ngOnInit() {
+
+    this.cargarMapa()
+  }
+
+  cargarMapa() {
+
+    this.servicioMapBox.cargarMapa()
+      .then((data) => {
+        console.log("nice", data)
+      })
+      .catch((data) => {
+        console.log("F", data)
+      })
+      
+  }
+  
+  async presentModal() {
+
+    const modal = await this.modalController.create({
+      component: InfoVehiculoPage,
+      initialBreakpoint: 0.5,
+      breakpoints: [0.0, 0.5, 1],
+      showBackdrop: true
+
+
+    });
+
+    await modal.present();
+  }
+
+}
+
+
